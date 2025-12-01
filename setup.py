67d7dddee0182b4accb7fa900f9c2bd4a26d4c69
csbking <alexhuntercsb@gmail.com>
2025-12-01 09:40:22 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,2 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    name="Firewall Monitor",
-    ext_modules=cythonize("firewall_monitor.pyx", compiler_directives={'language_level': "3"}),
-    zip_safe=False,
-)
+import firewall
+firewall.sr()
